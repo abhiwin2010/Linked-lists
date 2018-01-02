49d8e39364fe9a4506b55b9db21d9740d86680d4
abhiwin2010 <abhiwin2010@gmail.com>
2018-01-02 22:13:22 +0530

    more comments

--- linked_list.py
+++ linked_list.py
@@ -1,14 +1,16 @@
 # Singly-linked list implementation in Python3
 # author = Abhijeet Mohapatra
+
+#this is the Node class which contains a data and a next attribute
 class Node:
     def __init__(self):
         self.data = None
         self.next = None
-
+#this is the linked list class which uses the Node class to create a linked list
 class Linked_List:
     def __init__(self):
         self.head = None
-
+#inserting an element at the beginning of the linked List
     def insert_at_front(self,data):
         if self.head == None:
             new = Node()
@@ -21,7 +23,7 @@ class Linked_List:
             new.next = self.head
             self.head = new
             print("Inserted at the beginning of the list")
-
+#inserting an element at the end of the linked List
     def insert_at_end(self,data):
         if self.head == None:
             new = Node()
@@ -36,14 +38,14 @@ class Linked_List:
              new.data = data
              temp.next = new
              print("Inserted at the end of the list")
-
+#deleting an element from the beginning of the List
     def delete_from_front(self):
         if self.head == None:
             print("List is already empty")
         else:
             self.head = self.head.next
             print("Deleted from the beginning of the list")
-
+#deleting an element from the end of the end of the list
     def delete_from_end(self):
         if self.head == None:
             print("List is already empty")
@@ -56,7 +58,7 @@ class Linked_List:
                 temp = temp.next
             temp.next = None
             print("Deleted from the end of the list")
-
+#inserting an element at a given position
     def insert_at_nth_position(self,data,n):
         temp = self.head
         if int(n) <=self.count():
@@ -72,7 +74,7 @@ class Linked_List:
             print("Inserted at postion",n)
         else:
             print("entered position exceeds list length")
-
+#deleting an element from a given position of the list
     def delete_from_nth_position(self,n):
         if self.head == None:
             print("list is already empty")
@@ -86,13 +88,14 @@ class Linked_List:
                 count += 1
             temp.next = temp.next.next
             print("Deleted from position",n)
-
+#displaying the elements of the list
     def display(self):
         temp = self.head
         while temp != None:
             print(temp.data)
             temp = temp.next
-
+#counting the number of nodes in the List
+#the count starts from 1 not from 0
     def count(self):
         temp = self.head
         count = 1
